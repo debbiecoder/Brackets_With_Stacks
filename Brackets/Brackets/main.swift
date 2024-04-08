e39956d7242847c5f51fdf5b13d293dda4c17e4a
debbiecoder <129028696+debbiecoder@users.noreply.github.com>
2024-04-08 09:16:08 -0700

    Checking if balanced brackets works.
    Only accepts brackets, does not take into account other characters.

---
+++ Brackets/Brackets/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  Brackets
+//
+//  Created by Debbie Mathews on 4/1/24.
+//
+
+import Foundation
+
+var inputBrackets = "{[(])}]"
+var bracketsList = Array(inputBrackets)
+var lenBrackets = inputBrackets.count
+var yesesAndNos: [String] = []
+
+let openBrackets: [Character] = ["{", "[", "("]
+let closedBrackets: [Character] = ["}", "]", ")"]
+let openCloseBrackets: [Character: Character] = ["{":"}", "[":"]", "(":")"]
+
+
+var stack: [Character] = []
+func push(_ element: Character)
+{
+    stack.append(element)
+    print(stack)
+}
+func pop() -> Character?
+{
+    let stackLength = stack.count
+    
+    if stackLength <= 0
+    {
+        return nil
+    }
+    let removed = stack.remove(at: stackLength - 1)
+    return removed
+}
+
+
+for i in 0...lenBrackets - 1
+{
+    if openBrackets.contains(String(bracketsList[i])) == true
+    {
+        push(bracketsList[i])
+    }
+    else
+    {
+        if let remove = pop()
+        {
+            if openCloseBrackets[remove] == bracketsList[i]
+            {
+                yesesAndNos.append("yes")
+            }
+            else
+            {
+                yesesAndNos.append("no")
+            }
+        }
+        else
+        {
+            yesesAndNos.append("no")
+        }
+    }
+}
+if yesesAndNos.contains("no") == false
+{
+    print("It is balanced")
+}
+else
+{
+    print("It is not balanced")
+}
